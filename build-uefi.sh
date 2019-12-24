a07345f1f874c5c5f45bd51f8700696862b8e316
archfna <ad-min@mailbox.org>
2019-12-24 18:05:30 +0100

    build-uefi.sh: Fix invalid path

--- build-uefi.sh
+++ build-uefi.sh
@@ -6,7 +6,7 @@ device="t440p"
 filename="coreboot_tiano_${device}_archfan_`date +"%Y%m%d"`.rom"
 rm -f ~/dev/firmware/${filename}*
 rm -rf ./build
-cp configs/.config.${device}.uefi .config
+cp configs/config.${device}.uefi .config
 git submodule update --init --recursive
 make clean
 make olddefconfig
