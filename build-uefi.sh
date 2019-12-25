f5c2cca178606f8c41bb840524eb64ef33081ec3
archfna <ad-min@mailbox.org>
2019-12-25 01:30:58 +0100

    build-uefi.sh: Run "make crossgcc-i386" if build is older than 30 days.

--- build-uefi.sh
+++ build-uefi.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+CHECK_FILE="util/crossgcc/xgcc/bin/iasl"
+if [ ! -f "$CHECK_FILE"  ] || [ `find "$CHECK_FILE" -mtime +30 | egrep '.*'` ]; then
+	make crossgcc-i386 CPUS=$(nproc)
+else
+	echo "crossgcc is reasonably up-to-date. No need to waste precious cycles."
+fi
+
 device="t440p"
 filename="coreboot_tiano_${device}_archfan_`date +"%Y%m%d"`.rom"
 rm -f ~/dev/firmware/${filename}*
